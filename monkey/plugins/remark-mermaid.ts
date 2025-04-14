9490caeddfb6b296ee3af9d6454bd052bb43f395
spuuntries <kek@spuun.art>
2025-04-14 23:31:00 +0700

    feat!: add llada post

--- monkey/plugins/remark-mermaid.ts
+++ monkey/plugins/remark-mermaid.ts
@@ -21,6 +21,21 @@ export const mermaid: RemarkPlugin<[]> = () => (tree) => {
 
     // @ts-ignore
     node.type = 'html'
-    node.value = `<div class="mermaid" style="display: flex; align-items: center; justify-content: center;" data-content="${escapeHtml(node.value)}"><p>Loading graph...</p></div>`
+    node.value = `
+      <div class="mermaid" style="display: flex; align-items: center; justify-content: center;" data-content="${escapeHtml(node.value)}">
+        <div class="breathing-loader">
+          <span>L</span>
+          <span>o</span>
+          <span>a</span>
+          <span>d</span>
+          <span>i</span>
+          <span>n</span>
+          <span>g</span>
+          <span>.</span>
+          <span>.</span>
+          <span>.</span>
+        </div>
+      </div>
+    `
   })
 }
